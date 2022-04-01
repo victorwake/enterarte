5f87a93f1873634e96bf42af6cedda5e4a1b62e7
Victor Pinto <victorpintowake@gmail.com>
2022-03-31 21:56:58 -0300

    funciona registro y login

---
+++ src/main/java/com/enterarte/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.enterarte.controller;
+
+
+
+import com.enterarte.Service.CustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    private CustomerService customerService;
+
+
+    @GetMapping("/login")
+    public String loginForm(@RequestParam(required = false) String error, ModelMap model) {
+        if (error != null) {
+            model.put("error", "Email o password incorrectos");
+        }
+        return "/login/login";
+    }
+
+}
